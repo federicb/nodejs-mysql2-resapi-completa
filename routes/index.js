8d8ee8c7a795b21cd41ef7b6f0ca05cddd73420a
Federico <isma-fb@hotmail.com>
2023-02-01 13:58:28 +0100

    Endpoint funcionales

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.send('Holaa')
+});
+
+router.get('/employees', (req, res, next) => {
+  res.send('Obteniendo empleados')
+});
+
+router.post('/employees', (req, res, next) => {
+  res.send('Creando empleados')
+});
+
+router.put('/employees', (req, res, next) => {
+  res.send('Actualizando empleado')
+});
+
+router.delete('/employees', (req, res, next) => {
+  res.send('Eliminando  empleados')
 });
 
 module.exports = router;
