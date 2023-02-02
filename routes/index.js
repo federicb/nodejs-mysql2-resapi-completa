af3e2e5e13587a6a46405761601b4b8488a54e14
Federico <isma-fb@hotmail.com>
2023-02-02 09:54:44 +0100

    Rutas definidas correctamente

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../db.js')
+const employeesRoutes = require('./employees')
 
 /* GET home page. */
 router.get('/ping', async (req, res, next) => {
@@ -12,20 +13,6 @@ router.get('/', (req, res, next) => {
   res.send('Holaa')
 });
 
-router.get('/employees', (req, res, next) => {
-  res.send('Obteniendo empleados')
-});
-
-router.post('/employees', (req, res, next) => {
-  res.send('Creando empleados')
-});
-
-router.put('/employees', (req, res, next) => {
-  res.send('Actualizando empleado')
-});
-
-router.delete('/employees', (req, res, next) => {
-  res.send('Eliminando  empleados')
-});
+router.use(employeesRoutes)
 
 module.exports = router;
